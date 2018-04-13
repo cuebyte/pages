2962aa8cd7cf01ab1ece88dddab485ef936a8449
wei.huang <wei.huang@rakuten.de>
2018-04-13 23:22:09 +0200

    rebuilding site Fri Apr 13 23:22:09 CEST 2018

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,19 @@ git push origin master
 # Come Back up to the Project Root
 cd ..
 
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/a6329df49960fb45b6999fd2450d5d53/purge_cache" \
      -H "X-Auth-Email: william.hng@outlook.com" \
      -H "X-Auth-Key: 23bd88336beaee810d6c9871919e28e5a5f11" \
